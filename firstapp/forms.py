75e28fb5aef428089762c60ccf23d7be9ad989d7
Peskov Sergey <serpesgeikov@gmail.com>
2021-08-24 11:43:36 +0600

    crud (Person) - success

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -1,5 +1,17 @@
 from django import forms
 
+
 class UserForm(forms.Form):
-    name = forms.CharField(label="Имя")
-    age = forms.IntegerField(label="Возраст")
+    name = forms.CharField(
+        label="Имя",
+        max_length=30,
+        error_messages={"max_length": "Имя не должно превышать 30 символов"})
+
+    age = forms.IntegerField(
+        label="Возраст",
+        min_value=0,
+        max_value=200,
+        error_messages={
+            "min_value": "Возраст не должен быть отрицательным",
+            "max_value": "возраст не должен превышать 200 лет",
+            })
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render
 from .forms import UserForm
 from .models import Person
@@ -7,48 +7,59 @@ from .models import Person
 # получение данных из бд
 def index(request):
     people = Person.objects.all()
-    userform = UserForm()
-    return render(request, "firstapp/index.html", {
-        "form": userform,
-        "people": people
-    })
+    return render(request, "firstapp/index.html", {"people": people})
 
 
 # сохранение данных в бд
 def create(request):
     if request.method == "POST":
-        tom = Person()
-        tom.name = request.POST.get("name")
-        tom.age = request.POST.get("age")
-        tom.save()
-    return HttpResponseRedirect("/")
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            person = Person()
+            person.name = userform.cleaned_data["name"]
+            person.age = userform.cleaned_data["age"]
+            person.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "firstapp/create.html", {"form": userform})
+    else:
+        return render(request, "firstapp/create.html", {"form": UserForm()})
 
 
 def update(request, id):
-    if request.method == "POST":
-        tom = Person.objects.get(id=id)
-        tom.name = request.POST.get("name")
-        tom.age = request.POST.get("age")
-        tom.save(update_fields=["name", "age"])
-        return HttpResponseRedirect("/")
-    else:
-        userform = UserForm()
-        user = Person.objects.get(id=id)
-        return render(request, "firstapp/update.html", {"form": userform, "user": user})
+    try:
+        person = Person.objects.get(id=id)
+
+        if request.method == "POST":
+            userform = UserForm(request.POST)
+            if userform.is_valid():
+                person.name = userform.cleaned_data["name"]
+                person.age = userform.cleaned_data["age"]
+                person.save(update_fields=["name", "age"])
+                return HttpResponseRedirect("/")
+            else:
+                return render(request, "firstapp/create.html", {"form": userform})
+        else:
+            userform = UserForm(initial={
+                "name": person.name,
+                "age": person.age
+            })
+            return render(request, "firstapp/update.html", {"form": userform})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound("<h2>Person not found</h2>")
 
 
 def delete(request, id):
-    if request.method == "POST":
-        person = Person.objects.get(id=id)
-        person.delete()
-    return HttpResponseRedirect("/")
+    try:
+        Person.objects.filter(id=id).delete()
+        return HttpResponseRedirect("/")
+    except Person.DoesNotExist:
+        return HttpResponseNotFound("<h2>Person not found</h2>")
 
 
 def contact(request):
     # return HttpResponseRedirect("/products")
     users = Person.objects.all()
-
-    users
 
 
 def details(request):
