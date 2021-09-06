8f190f7cdc64eb078020f1384dfc1821e3323faf
Peskov Sergey <serpesgeikov@gmail.com>
2021-09-06 18:05:30 +0600

    login

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     path('login/', views.login),
     path('logout/', views.login),
     path('register/', views.register),
-    path('create/', views.createUser),
-    path('update/', views.updateUser),
-    path('delete/', views.deleteUser),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,91 +1,53 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, logout
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        # email = regForm.cleaned_data["email"]
+        password = request.POST.get("password")
+        user = User.objects.create_user(username, username, password)
+        # login(request, user)
+        return HttpResponseRedirect("/")
+        # regForm = UserCreationForm(request.POST)
+        # if regForm.is_valid():
+        #     username = regForm.cleaned_data["username"]
+        #     # email = regForm.cleaned_data["email"]
+        #     password = regForm.cleaned_data["password"]
+        #     user = User.objects.create_user(username, password)
+        #     login(request, user)
+        #     return HttpResponseRedirect("/")
+        # else:
+        #     return render(request, "authentication/register.html", {"form": regForm})
+    else:
+        return render(request, "authentication/register.html", {"form": UserCreationForm()})
 
 def login(request):
     if request.method == "POST":
-        userForm = UserForm_name(request.POST)
-        if userForm.is_valid():
-            username = userForm.cleaned_data["username"]
-            password = userForm.cleaned_data["password"]
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponseRedirect("/")
-                else:
-                    return HttpResponse("Вы деактивировали ваш аккаунт")
+        authForm = AuthenticationForm(request.POST)
+        # if authForm.is_valid():
+            # username = authForm.cleaned_data["username"]
+            # password = authForm.cleaned_data["password"]
+        username = request.POST.get("username")
+        # email = regForm.cleaned_data["email"]
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect("/")
+            else:
+                return HttpResponse("Вы деактивировали ваш аккаунт")
         else:
-            return render(request, "authentication/login.html", {"form": userForm})
+            return render(request, "authentication/login.html", {"form": authForm})
     else:
-        return render(request, "authentication/login.html", {"form": UserForm()})
+        return render(request, "authentication/login.html", {"form": AuthenticationForm()})
 
 
 def logout(request):
     logout(request)
     # Redirect to a success page.
     return HttpResponseRedirect("/")
-
-def register(request):
-    return HttpResponseRedirect("/")
-
-def create(request):
-    user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
-    return HttpResponseRedirect("/")
-
-def update(request):
-    request.user.save()
-    return HttpResponseRedirect("/")
-
-def delete(request):
-    return HttpResponseRedirect("/")
-
-# Create your views here.
-# Создание нового пользователя
-# def createUser(request):
-#     if request.method == "POST":
-#         userForm = UserForm(request.POST)
-#         if userForm.is_valid():
-#             person = Person()
-#             person.name = userForm.cleaned_data["name"]
-#             person.age = userForm.cleaned_data["age"]
-#             person.save()
-#             return HttpResponseRedirect("/")
-#         else:
-#             return render(request, "firstapp/createUser.html", {"form": userForm})
-#     else:
-#         return render(request, "firstapp/createUser.html", {"form": UserForm()})
-
-
-# # Обновление пользователя
-# def updateUser(request, id):
-#     try:
-#         person = Person.objects.get(id=id)
-
-#         if request.method == "POST":
-#             userForm = UserForm(request.POST)
-#             if userForm.is_valid():
-#                 person.name = userForm.cleaned_data["name"]
-#                 person.age = userForm.cleaned_data["age"]
-#                 person.save(update_fields=["name", "age"])
-#                 return HttpResponseRedirect("/")
-#             else:
-#                 return render(request, "firstapp/updateUser.html", {"form": userForm})
-#         else:
-#             userForm = UserForm(initial={
-#                 "name": person.name,
-#                 "age": person.age
-#             })
-#             return render(request, "firstapp/updateUser.html", {"form": userForm})
-#     except Person.DoesNotExist:
-#         return HttpResponseNotFound("<h2>Person not found</h2>")
-
-
-# # Удаление пользователя
-# def deleteUser(request, id):
-#     try:
-#         Person.objects.filter(id=id).delete()
-#         return HttpResponseRedirect("/")
-#     except Person.DoesNotExist:
-#         return HttpResponseNotFound("<h2>Person not found</h2>")
